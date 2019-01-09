e347f7ae5973b3a801c6e8ed5e65f0b06c36540d
Volodymyr Partyka <volodymyr.partyka@teamvoy.com>
2019-01-09 23:12:53 +0200

    add android version, removed unnessesary code

--- App.js
+++ App.js
@@ -7,97 +7,54 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Image } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import RNFS from "react-native-fs";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
+const rootPath = Platform.select({
+  ios: RNFS.DocumentDirectoryPath,
+  android: `${RNFS.ExternalStorageDirectoryPath}/SampleReactDir`
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  state = {
-    error: "",
-    testPath: ""
-  };
+  state = { error: null };
 
   componentDidMount() {
-    this.createFolder();
+    Platform.OS === "ios" ? this.createFiles() : this.createFolderAndFiles();
   }
-  createFolder = () => {
-    // var path = `~/Documents/Pages/test1.txt`;
-    // RNFS.writeFile(path, "Lorem ipsum dolor sit amet", "utf8")
-    //   .then(success => this.setState({ testPath: path }))
-    //   .catch(err => this.setState({ error: "File" + err.message }));
-    // const dirPath = "~/Documents" + "/CreateSampleDir";
-    // RNFS.mkdir(dirPath)
-    //   .then(success => {
-    //     const fileNames = ["test1.png"];
-    //     fileNames.forEach(fileName => {
-    //       var path = `${dirPath}/${fileName}`;
-    //       RNFS.writeFile(path, "Lorem ipsum dolor sit amet", "utf8")
-    //         .then(success => this.setState({ testPath: path }))
-    //         .catch(err => this.setState({ error: "File" + err.message }));
-    //     });
-    //   })
-    //   .catch(err => this.setState({ error: "DIR" + err.message }));
+
+  createFiles = () => {
+    const fileNames = ["test1.txt", "test2.txt"];
+
+    fileNames.forEach(fileName => {
+      let path = `${rootPath}/${fileName}`;
+      RNFS.writeFile(path, "Lorem ipsum dolor sit amet", "utf8").catch(err =>
+        this.setState({ error: "File: " + err.message })
+      );
+    });
   };
 
-  onDownloadImagePress() {
-    const dirPath = RNFS.DocumentDirectoryPath + "/CreateSampleDir";
-    RNFS.mkdir(dirPath).then(success => {
-      RNFS.downloadFile({
-        fromUrl: "https://facebook.github.io/react-native/img/header_logo.png",
-        toFile: `${RNFS.DocumentDirectoryPath}/CreateSampleDir/react-native.png`
-      })
-        .promise.then(r => {
-          this.setState({ isDone: true });
-        })
-        .catch(err => this.setState({ error: "File" + err.message }));
-    });
+  createFolderAndFiles = () => {
+    RNFS.mkdir(rootPath)
+      .then(() => this.createFiles())
+      .catch(err => this.setState({ error: "DIR: " + err.message }));
+  };
+
+  get errors() {
+    const { error } = this.state;
+
+    return error ? (
+      <Text style={styles.instructions}>Errors: {error}</Text>
+    ) : null;
   }
 
   render() {
-    const preview = this.state.isDone ? (
-      <View>
-        <Image
-          style={{
-            width: 100,
-            height: 100,
-            backgroundColor: "black"
-          }}
-          source={{
-            uri: `file://${
-              RNFS.DocumentDirectoryPath
-            }/CreateSampleDir/react-native.png`,
-            scale: 1
-          }}
-        />
-        <Text>{`file://${
-          RNFS.DocumentDirectoryPath
-        }/CreateSampleDir/react-native.png`}</Text>
-      </View>
-    ) : null;
-
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>{RNFS.DocumentDirectoryPath}</Text>
-        {preview}
-        <Text style={styles.welcome}>isDone: {this.state.isDone}</Text>
-        <Text style={styles.welcome}>error: {this.state.error}</Text>
-        <Text style={styles.welcome}>testPath: {this.state.testPath}</Text>
-        <Text onPress={() => this.onDownloadImagePress()}>Download Image</Text>
-        <Image
-          style={{ width: 100, height: 100 }}
-          source={{
-            uri: "~/Documents/Keynote/img.JPG",
-            scale: 1
-          }}
-        />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>rootPath: {rootPath}</Text>
+        <Text style={styles.instructions}>Now open the Files app</Text>
+        {this.errors}
       </View>
     );
   }
